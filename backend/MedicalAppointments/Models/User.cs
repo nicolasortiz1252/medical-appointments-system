564a7c7847b7e78a9da6876aabf474a9b0e1b58d
nico1252 <matias.nicolas.ortiz@gmail.com>
2026-05-21 23:40:29 -0300

    feat: Creacion de la base de datos y tablas

---
+++ backend/MedicalAppointments/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public string Role { get; set; } = "Patient";
+}
