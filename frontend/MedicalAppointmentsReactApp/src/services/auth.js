3e22b9ad03c92d91ab801c9c4e9864d2528a5dba
Alex <alexftf_xd@hotmail.com>
2026-07-20 17:08:53 -0300

    configuro url de api con variable de entorno

--- frontend/MedicalAppointmentsReactApp/src/services/auth.js
+++ frontend/MedicalAppointmentsReactApp/src/services/auth.js
@@ -1,6 +1,6 @@
 export async function login(username, password) {
   
-  const response = await fetch("https://dummyjson.com/auth/login", {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
     
     method: "POST",
     headers: {
